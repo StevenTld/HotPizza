2c6260f2aee10cf047bbfb14137f43cee3c0d3e1
Clement <clement.thomas22540@gmail.com>
2025-03-11 11:21:43 +0100

    Init Spring

---
+++ Projet_S8_Spring/src/main/java/com/example/projet_s8_spring/ProjetS8SpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.projet_s8_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetS8SpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjetS8SpringApplication.class, args);
+    }
+
+}
---
+++ Projet_S8_Spring/src/test/java/com/example/projet_s8_spring/ProjetS8SpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projet_s8_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetS8SpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
