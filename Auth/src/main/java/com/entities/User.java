8eedc47d481786cd40847eff0b64b36b3f2dca20
Steven <116876919+StevenTld@users.noreply.github.com>
2025-03-16 18:37:18 +0100

    Ajout de l'api d'authentification

---
+++ Auth/src/main/java/com/entities/User.java
@@ -0,0 +1,24 @@
+package com.entities;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "app_user") // Renommage de la table pour éviter le conflit avec le mot-clé MySQL
+@Data
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // Ajout de l'auto-incrément
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+
+
+
+}
